78f106e50690e68e95a358800626072d3ab98e7d
tkyg <80114433+tkyg@users.noreply.github.com>
2022-07-08 18:19:05 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState("")
+  useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setImage(data.message))
+  }, [])
+
+  if(!image){
+    return<p>Loading...</p>
+  }
+
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  )
+}
+export default App;
